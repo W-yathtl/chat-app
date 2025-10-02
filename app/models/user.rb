0426e5c48feee0c5c50623f16c01df8f54f35463
W-yathtl <dopin9panda@gmail.com>
2025-10-02 15:58:18 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 名前がないと保存しないバリデーション
+  validates :name, presence: true
 end
