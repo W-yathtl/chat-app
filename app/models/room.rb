473b902519efc77a83809d0399a7811cf1ef5676
W-yathtl <dopin9panda@gmail.com>
2025-10-02 17:18:00 +0900

    チャット画面の編集

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,9 @@ class Room < ApplicationRecord
   has_many :room_users
   # 多くのusersと紐づくアソシエーションをroom_usersを中間モデルに定義
   has_many :users, through: :room_users
+
+  # 名前がないと保存しないバリデーション
+  validates :name, presence: true
+
+  
 end
