001b83ff61b8a1a1954eea6f59307d64797b7904
W-yathtl <dopin9panda@gmail.com>
2025-10-04 08:42:34 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ApplicationRecord
   # メッセージはroom_idとuser_idのアソシエーションを定義する
   belongs_to :room
   belongs_to :user
+
+  # メッセージ内容がないと保存しないバリデーション
+  validates :content, presence: true
 end
